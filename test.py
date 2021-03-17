f1a7c1453e41f647bdde3ed7910bbe0334e907fd
hato44ekira <ahmedtariqyork@gmail.com>
2021-03-17 15:50:37 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import json
+import sys
+
+with open(sys.argv[1]) as json_data:
+	data = json.load(json_data)
+	print(data)
+	from pandas import json_normalize
+	finaldata=json_normalize(data)
+	
+	print(finaldata)
+	json1=finaldata.reset_index().to_dict(orient='records')
+
+
+	[newjson]=json1
+	print(newjson)
+	del newjson['index']
+	print(type(newjson))
+	with open('result.json', 'w') as fp:
+		json.dump(newjson, fp)
