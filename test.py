f4b78b2bcb35552fc859c2a9513e1f9db8ad5676
hato44ekira <ahmedtariqyork@gmail.com>
2021-03-17 16:02:48 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,21 @@
 import json
 import sys
+if len(sys.argv)>1:
+	with open(sys.argv[1]) as json_data:
+		data = json.load(json_data)
+		print(data)
+		from pandas import json_normalize
+		finaldata=json_normalize(data)
+		
+		print(finaldata)
+		json1=finaldata.reset_index().to_dict(orient='records')
 
-with open(sys.argv[1]) as json_data:
-	data = json.load(json_data)
-	print(data)
-	from pandas import json_normalize
-	finaldata=json_normalize(data)
-	
-	print(finaldata)
-	json1=finaldata.reset_index().to_dict(orient='records')
 
-
-	[newjson]=json1
-	print(newjson)
-	del newjson['index']
-	print(type(newjson))
-	with open('result.json', 'w') as fp:
-		json.dump(newjson, fp)
+		[newjson]=json1
+		print(newjson)
+		del newjson['index']
+		print(type(newjson))
+		with open('result.json', 'w') as fp:
+			json.dump(newjson, fp)
+else:
+	print("please enter required arguments")
